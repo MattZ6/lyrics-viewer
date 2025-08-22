ceb5da9a5998d3a83ae8a6d2082cba35ca67b15d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-08-22 14:46:54 -0300

    refactor(home): removes initial track initialization

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,3 @@
-import { useEffect } from "react";
-import { useSetAtom } from "jotai";
-
-import { currentTrackAtom } from "@/atoms/player";
-
-import { tracks } from "@/data/tracks";
-
 import { SegmentsPanel } from "@/components/segments-panel";
 import { Controls } from "@/components/controls";
 
@@ -13,12 +6,6 @@ import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 
 export function HomePage() {
-  const setTrack = useSetAtom(currentTrackAtom)
-
-  useEffect(() => {
-    setTrack(tracks[0])
-  }, [setTrack])
-
   return (
     <BackgroundImage>
       <main
