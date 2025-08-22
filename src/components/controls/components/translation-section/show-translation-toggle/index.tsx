93bb1f73d75b6dfd5e8dcb61fc99f66af848ef91
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-08-22 14:19:11 -0300

    feat(controls): add translation toggle (#10)

---
+++ src/components/controls/components/translation-section/show-translation-toggle/index.tsx
@@ -0,0 +1,20 @@
+import { useAtom } from "jotai";
+
+import { showTranslatedTextAtom } from "@/atoms/segment-view";
+
+import { Switch } from "@/components/ui/switch";
+
+export function ShowTranslationToggle() {
+  const [showTranslation, toggleTranslationVisibility] = useAtom(showTranslatedTextAtom)
+
+  return (
+    <label className="flex items-center justify-between">
+      <span className="text-sm text-white/72">Show translation</span>
+      <Switch
+        className="cursor-pointer"
+        checked={showTranslation}
+        onCheckedChange={toggleTranslationVisibility}
+      />
+    </label>
+  )
+}
