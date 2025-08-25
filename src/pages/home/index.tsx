c56dcd791d7e6df18a22bfb99bdfd15f92d67a73
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-08-25 14:09:30 -0300

    chore: remove background image (#17)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,19 @@
 import { SegmentsPanel } from "@/components/segments-panel";
 import { Controls } from "@/components/controls";
 
-import { BackgroundImage } from "./components/background-image";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
 
 export function HomePage() {
   return (
-    <BackgroundImage>
-      <main
-        className="flex flex-col h-[100dvh] bg-black/50 backdrop-blur-[48px]"
-      >
-        <Header />
+    <div className="flex flex-col h-[100dvh] bg-black">
+      <Header />
 
-        <SegmentsPanel />
+      <SegmentsPanel />
 
-        <Controls />
+      <Controls />
 
-        <Footer />
-      </main>
-    </BackgroundImage>
+      <Footer />
+    </div>
   )
 }
