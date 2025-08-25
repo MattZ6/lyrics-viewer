4c5e5fdcc36cfd5d0db6521053d9ef69dfebf46c
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-08-25 15:12:13 -0300

    feat(config): adds app name to env file

--- src/pages/home/components/header/components/brand/index.tsx
+++ src/pages/home/components/header/components/brand/index.tsx
@@ -1,10 +1,12 @@
+import { env } from "@/config/env";
+
 import { AnimatedLogo } from "./components/animated-logo";
 
 export function Brand() {
   return (
     <div className="flex items-center gap-2 text-white">
       <AnimatedLogo size={24} />
-      <span className="font-medium text-base select-none">Lyrics Viewer</span>
+      <span className="font-medium text-base select-none">{env.appName}</span>
     </div>
   )
 }
