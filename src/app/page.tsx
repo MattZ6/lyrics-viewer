1b59bc38570846a019fb1cef5b3a6f7e3aba9c12
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-08-29 11:57:16 -0300

    Refactor/remove unused pages (#27)
    * refactor: deletes app component
    * refactor: deletes pages folder
    * refactor(home): adds components structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import { HomePage as HomePageContent } from "@/pages/home";
+import { Controls } from "@/components/controls";
+import { Header } from "@/components/header";
+import { Player } from "@/components/player";
+import { SegmentsPanel } from "@/components/segments-panel";
 
 export default function HomePage() {
-  return <HomePageContent />;
+  return (
+    <div className="flex flex-col h-[100dvh] ">
+      <Header />
+
+      <SegmentsPanel />
+
+      <Controls />
+
+      <Player />
+    </div>
+  );
 }
