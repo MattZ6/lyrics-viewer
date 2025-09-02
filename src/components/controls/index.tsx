180cb3715ac62f58d3811ff9313a1a9398a60430
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-09-02 17:15:22 -0300

    chore(controls): improves position on mobile

--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -7,7 +7,7 @@ export function Controls() {
     <div
       className={cn(
         "flex flex-col gap-1 p-1 rounded-full bg-gradient-to-tr from-zinc-900 to-zinc-800 border border-zinc-800 z-10",
-        "absolute right-4 bottom-0 top-0 mb-[144px] self-center",
+        "absolute right-4 bottom-[124px] md:bottom-0 md:top-0 md:mb-[144px] self-center",
       )}
     >
       <SettingsMenuControl />
