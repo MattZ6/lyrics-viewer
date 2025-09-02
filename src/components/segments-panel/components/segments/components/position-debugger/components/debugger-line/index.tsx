2432f6e69ddb29ebb86fb4f9847fa0e5def23b2a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-09-02 17:17:28 -0300

    chore(debugger-line): improves styles on mobile

--- src/components/segments-panel/components/segments/components/position-debugger/components/debugger-line/index.tsx
+++ src/components/segments-panel/components/segments/components/position-debugger/components/debugger-line/index.tsx
@@ -22,9 +22,9 @@ export function DebuggerLine({ scrollableContainerRef }: Props) {
   return (
     <div
       ref={debuggerRef}
-      className="absolute left-0 right-0 w-full h-0 shrink-0 border-t border-cyan-500/50 text-cyan-500 border-dashed z-10 duration-250 transition-[top] px-4 py-1"
+      className="absolute left-0 right-0 w-full h-0 shrink-0 border-t border-white/24 border-dashed z-10 duration-200 transition-[top] px-4 py-1"
     >
-      Scroll anchor position ({scrollAnchor * 100}%)
+      <span className="hidden md:block select-none text-white/56 text-sm">Scroll anchor position ({scrollAnchor * 100}%)</span>
     </div>
   );
 }
