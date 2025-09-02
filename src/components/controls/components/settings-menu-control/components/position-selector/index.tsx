d7f29531c8aa5d28a272a49ec5765ccec50845ac
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-09-02 16:39:18 -0300

    feat(controls): improves scroll settings menu

--- src/components/controls/components/settings-menu-control/components/position-selector/index.tsx
+++ src/components/controls/components/settings-menu-control/components/position-selector/index.tsx
@@ -12,14 +12,17 @@ export function PositionSelector() {
 
   return (
     <div className="flex flex-col gap-2">
-      <span className="text-sm text-zinc-400">Position</span>
+      <span className="text-sm text-white font-medium selection:bg-white selection:text-black">
+        Scroll anchor position
+      </span>
       <div className="flex items-center self-start p-1 rounded-full bg-zinc-700/50">
         {OPTIONS.map((value) => (
           <button
             key={value}
             type="button"
+            aria-label={`Set scroll anchor to ${value * 100}% of the viewport`}
             className={cn(
-              "flex items-center justify-center rounded-full text-sm px-2 py-1 not-disabled:cursor-pointer text-zinc-100",
+              "flex items-center justify-center rounded-full text-sm px-2 py-1 not-disabled:cursor-pointer text-zinc-100 select-none outline-none focus-visible:ring-[2px] focus-visible:border-ring focus-visible:ring-zinc-500/50",
               value === anchor && "bg-zinc-900 text-zinc-100",
             )}
             onClick={() => setAnchor(value)}
--- src/components/controls/index.tsx
+++ src/components/controls/index.tsx
@@ -1,14 +1,16 @@
 import { cn } from "@/lib/utils";
+import { SettingsMenuControl } from "./components/settings-menu-control";
 import { TranslationToggleControl } from "./components/translation-toggle-control";
 
 export function Controls() {
   return (
     <div
       className={cn(
-        "flex flex-col gap-2 p-1 rounded-full bg-zinc-800 border border-zinc-700",
+        "flex flex-col gap-1 p-1 rounded-full bg-gradient-to-tr from-zinc-900 to-zinc-800 border border-zinc-800 z-10",
         "absolute right-4 bottom-0 top-0 mb-[144px] self-center",
       )}
     >
+      <SettingsMenuControl />
       <TranslationToggleControl />
     </div>
   );
