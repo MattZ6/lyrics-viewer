2207cea105fabbe251a0e60290b4bbd4591b3649
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-09-03 14:08:51 -0300

    feat(audio-loader): adds cache layer

--- src/components/player/components/audio-loader/index.tsx
+++ src/components/player/components/audio-loader/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAtomValue, useSetAtom } from "jotai";
-import { type SyntheticEvent, useCallback } from "react";
+import { type SyntheticEvent, useCallback, useEffect, useState } from "react";
 
 import {
   audioRefAtom,
@@ -9,6 +9,7 @@ import {
   currentTrackAtom,
   durationAtom,
 } from "@/atoms/player";
+import { audioCacheManager } from "@/services/cache-audio-manager";
 
 type TimeUpdatedEvent = SyntheticEvent<HTMLAudioElement, Event>;
 type MetadataLoadedEvent = SyntheticEvent<HTMLAudioElement, Event>;
@@ -18,6 +19,9 @@ export function AudioLoader() {
   const setAudioRef = useSetAtom(audioRefAtom);
   const setCurrentTime = useSetAtom(currentTimeAtom);
   const setDuration = useSetAtom(durationAtom);
+
+  const [audio, setAudio] = useState<Blob | null>(null);
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
 
   const handleSetAudioRef = useCallback(
     (element: HTMLAudioElement | null) => {
@@ -42,7 +46,46 @@ export function AudioLoader() {
     [setDuration],
   );
 
-  if (!track) {
+  useEffect(() => {
+    if (audio) {
+      if (!(audio instanceof Blob)) {
+        // TODO: Handle error
+        return;
+      }
+
+      const objectUrl = URL.createObjectURL(audio);
+
+      setFileUrl(objectUrl);
+
+      return () => {
+        URL.revokeObjectURL(objectUrl);
+      };
+    }
+  }, [audio]);
+
+  useEffect(() => {
+    async function loadAudioFile() {
+      try {
+        let blob = await audioCacheManager.getBlobFromCache(track.audio);
+
+        if (!blob) {
+          blob = await fetch(track.audio).then((response) => response.blob());
+
+          if (blob) {
+            await audioCacheManager.saveBlobToCache(track.audio, blob);
+          }
+        }
+
+        setAudio(blob);
+      } catch {
+        // TODO: Handle error
+      }
+    }
+
+    loadAudioFile();
+  }, [track.audio]);
+
+  if (!fileUrl) {
     return null;
   }
 
@@ -50,8 +93,9 @@ export function AudioLoader() {
     // biome-ignore lint/a11y/useMediaCaption: It doesn't have captions
     <audio
       ref={handleSetAudioRef}
-      src={track.audio}
+      src={fileUrl}
       translate="no"
+      controls={false}
       onTimeUpdate={handleTimeUpdate}
       onLoadedMetadata={handleMetadataLoaded}
       preload="metadata"
