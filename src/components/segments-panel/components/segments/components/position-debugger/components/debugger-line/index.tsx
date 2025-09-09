6600ec2ce24caa62f1fc5e4be758ca04662b568c
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-09-09 10:29:14 -0300

    chore: lint files

--- src/components/segments-panel/components/segments/components/position-debugger/components/debugger-line/index.tsx
+++ src/components/segments-panel/components/segments/components/position-debugger/components/debugger-line/index.tsx
@@ -24,7 +24,9 @@ export function DebuggerLine({ scrollableContainerRef }: Props) {
       ref={debuggerRef}
       className="absolute left-0 right-0 w-full h-0 shrink-0 border-t border-white/24 border-dashed z-10 duration-200 transition-[top] px-4 py-1"
     >
-      <span className="hidden md:block select-none text-white/56 text-sm">Scroll anchor position ({scrollAnchor * 100}%)</span>
+      <span className="hidden md:block select-none text-white/56 text-sm">
+        Scroll anchor position ({scrollAnchor * 100}%)
+      </span>
     </div>
   );
 }
