a4c19768284a02a97720e82cb4735101169e8ce2
Matheus Felipe Zanin <matt_z6@hotmail.com>
2025-10-01 13:35:22 -0300

    feat(header): animates logo while music is playing

--- src/components/header/components/brand/index.tsx
+++ src/components/header/components/brand/index.tsx
@@ -1,11 +1,26 @@
-import { env } from "@/config/env";
+"use client";
 
-import { AnimatedLogo } from "./components/animated-logo";
+import { useAtomValue } from "jotai";
+import { useEffect, useRef } from "react";
+import { isPlayingAtom } from "@/atoms/player";
+import { env } from "@/config/env";
+import { AnimatedLogo,type AnimatedLogoHandle } from "./components/animated-logo";
 
 export function Brand() {
+  const animatedLogoRef = useRef< AnimatedLogoHandle>(null);
+  const isPlaying = useAtomValue(isPlayingAtom);
+
+  useEffect(() => {
+    if(isPlaying) {
+      animatedLogoRef.current?.startAnimation()
+    }else {
+      animatedLogoRef.current?.stopAnimation()
+    }
+  }, [isPlaying]);
+
   return (
     <div className="flex items-center gap-2 text-white">
-      <AnimatedLogo size={24} />
+      <AnimatedLogo ref={animatedLogoRef} size={24} />
       <span className="font-medium text-base select-none">{env.appName}</span>
     </div>
   );
